bb86c2258109f738ecf393e6e99ecd42943b81df
Denys Bartosevych <denys.bartosevych.pz.2021@lpnu.ua>
2023-05-19 19:19:05 +0300

    index page: table, datepicker, stats front table page small changes addedit

---
+++ Data/FilterService.cs
@@ -0,0 +1,9 @@
+﻿namespace Finance_Manager.Data;
+
+public static class FilterService
+{
+    public static bool OperationType { get; set; }
+    public static string Type { get; set; }
+    public static DateTime firstDate { get; set; }
+    public static DateTime lastDate { get; set; }
+}
--- Data/FinanceOperation.cs
+++ Data/FinanceOperation.cs
@@ -39,35 +39,4 @@ namespace Finance_Manager.Data
         Credit,
         Other
     }
-
-    public abstract class FinanceOperation
-    {
-        public decimal Amount { get; set; }
-        public string Description { get; set; }
-        public DateTime? Date { get; set; }
-        public FinanceOperation(decimal amount = 0m, DateTime? date = null, string description = "")
-        {
-            Amount = amount;
-            Date = date == null ? DateTime.Today : date;
-            Description = description;
-        }
-    }
-    public class Income : FinanceOperation
-    {
-        public IncomeType Type { get; set; }
-        public Income(decimal amount = 0m, IncomeType type = IncomeType.Other, DateTime? date = null, string description = "")
-            : base(amount, date, description)
-        {
-            Type = type;
-        }
-    }
-    public class Spends : FinanceOperation
-    {
-        public SpendType Type { get; set; }
-        public Spends(decimal amount = 0m, SpendType type = SpendType.Other, DateTime? date = null, string description = "")
-            : base(amount, date, description)
-        {
-            Type = type;
-        }
-    }
 }
